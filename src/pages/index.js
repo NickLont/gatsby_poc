5d920c6813da0574d51fb9137820bca1ee4d4451
Nick Lontorfos <nlontorfos@hotmail.com>
2021-03-16 13:51:25 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Home = () => {
+  return (
+    <div style={{ color: "purple" }}>
+      <Link to="/contact/">Contact</Link>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
+
+export default Home
